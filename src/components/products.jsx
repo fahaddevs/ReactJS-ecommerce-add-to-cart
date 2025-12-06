b3b3a8f7f1e034686ce71f820b141fc831878dae
fahaddevs <fahaddevs@gmail.com>
2025-12-06 08:06:22 +0600

    Add products list

---
+++ src/components/products.jsx
@@ -0,0 +1,35 @@
+
+export default function Products({ products }) {
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</h2>
+
+        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {products.map((product) => (
+            <div key={product.id} className="group relative">
+              <img
+                alt={product.imageAlt}
+                src={product.imageSrc}
+                className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
+              />
+              <div className="mt-4 flex justify-between">
+                <div className="text-left">
+                  <h3 className="text-sm text-gray-700">
+                    <a href={product.href} className="font-semibold text-[16px]">
+                      <span aria-hidden="true" className="absolute inset-0" />
+                      {product.name}
+                    </a>
+                  </h3>
+                  <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+                </div>
+                <p className="text-sm font-medium text-gray-900">{product.price}</p>
+              </div>
+              <button type="button" className="flex items-center content-center bg-black text-white font-semibold rounded-lg cursor-pointer py-2 px-10 mt-2">Add Cart</button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
